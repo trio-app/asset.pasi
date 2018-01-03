cc6b17c415fd1666d8f5b3e54f504442f6d714a9
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2018-01-03 15:23:05 +0700

    Edit Asset Group

--- application/APP_CI/modules/assetgroup/controllers/assetgroup.php
+++ application/APP_CI/modules/assetgroup/controllers/assetgroup.php
@@ -2,43 +2,34 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class assetgroup extends MX_Controller {
-
-    public function index(){
-       $this->load->view('V_assetgroup');
-    }
     
     public function read(){
-        $this->load->model('R_assetgroup');
+        $this->load->model('Rassetgroup');
         $start = $this->input->post('start');
         $limit = $this->input->post('limit');
         $filter = $this->input->post('filter');
         header('Content-type: application/json');
-        print_r( $this->R_assetgroup->load_default($start,$limit,$filter));
+        print_r( $this->Rassetgroup->load_default($start,$limit,$filter));
         
     }
 
     public function create(){   
         $jsonData =  file_get_contents("php://input");
-        $this->load->model('C_assetgroup');
-        $hasil = $this->C_assetgroup->insertDT(json_decode($jsonData,true));
+        $this->load->model('Cassetgroup');
+        $hasil = $this->Cassetgroup->insertDT(json_decode($jsonData,true));
         print($hasil); 
     }
     public function update(){
         $jsonData =  file_get_contents("php://input");        
-        $this->load->model('U_assetgroup');
-        $hasil = $this->U_assetgroup->updateDT(json_decode($jsonData,true));
+        $this->load->model('Uassetgroup');
+        $hasil = $this->Uassetgroup->updateDT(json_decode($jsonData,true));
         print($hasil);
     }
     public function delete(){
         $jsonData =  file_get_contents("php://input");        
-        $this->load->model('D_assetgroup');
-        $hasil = $this->D_assetgroup->deleteDT(json_decode($jsonData,true));
+        $this->load->model('Dassetgroup');
+        $hasil = $this->Dassetgroup->deleteDT(json_decode($jsonData,true));
         print($hasil);
-    }
-    public function cbolist(){
-        $this->load->model('R_assetgroup');
-        header('Content-type: application/json');
-        print_r($this->R_assetgroup->cbolist());
     }
             
 }
