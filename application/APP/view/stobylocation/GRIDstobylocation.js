109daa2d62d0a5f9a7d53b02b16cc108442deb3c
Fahmi Alfath <fahmi243alfath@gmail.com>
2018-01-04 22:37:35 +0700

    Bug Fix

--- application/APP/view/stobylocation/GRIDstobylocation.js
+++ application/APP/view/stobylocation/GRIDstobylocation.js
@@ -15,16 +15,6 @@ Ext.define('TDK.view.stobylocation.GRIDstobylocation', {
         {header: "Asset Location",dataIndex: 'AssetLocation',width: 200,sortable: true,filter:'string'},
         {header: "Total Asset",dataIndex: 'TotalAsset',width: 200,sortable: true,filter:'string'},
         {header: "Scanned",dataIndex: 'AssetScanned',flex: 1,sortable: true,filter:'string'},
-        {header: "Not Scanned",dataIndex: 'AssetNotScan',flex: 1,sortable: true,filter:'string'}
-        
-    ],
-
-    bbar: {
-        xtype: 'pagingtoolbar',
-        displayInfo: true,
-        displayMsg: 'Displaying topics {0} - {1} of {2}',
-        emptyMsg: "No topics to display",
-
-    }
-    
+        {header: "Not Scanned",dataIndex: 'AssetNotScan',flex: 1,sortable: true,filter:'string'}   
+    ]
 });
